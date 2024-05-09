ec92ebff4f59fd60621a1cc070b73625061d6b1a
aekapitsa <a.kapitsa@yahoo.com>
2024-05-09 08:14:07 +0300

    AEK_fix_realisation of 1 by 1 opening

--- src/components/AccordionItem.js
+++ src/components/AccordionItem.js
@@ -6,10 +6,13 @@ function AccordionItem({
   handleSetActiveElement,
 }) {
   return (
-    <div className="item" onClick={() => handleSetActiveElement(number)}>
+    <div
+      className={`item ${activeElement === number ? "open" : ""}`}
+      onClick={() => handleSetActiveElement(number)}
+    >
       <p className="number">{number < 9 ? `0${number + 1}` : number + 1}</p>
       <p className="title">{title}</p>
-      <p className="icon">+</p>
+      <p className="icon">{activeElement === number ? "-" : "+"}</p>
       {activeElement === number ? (
         <div className="content-box">{text}</div>
       ) : null}
